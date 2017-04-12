4cb380668799edbd7171af048a8f120feca8a12d
Kyle Helle <kjhelle4@gmail.com>
2017-04-12 11:52:49 -0700

    Add GetStatus method

--- UsingCmakeJs/Peripherals/src/id_scanner.cpp
+++ UsingCmakeJs/Peripherals/src/id_scanner.cpp
@@ -1,5 +1,6 @@
 #include <nan.h>
 #include <iostream>
+#include <vector>
 #include "id_scanner.h"
 
 using namespace std;
@@ -23,6 +24,27 @@ NAN_METHOD(PassString) {
             Nan::New<String>(str.c_str()).ToLocalChecked());
 }
 
+string getStatusMessage(int statusCode, vector<string>& messages) {
+    // TODO: error checking here...
+    return messages[statusCode];
+}
+
+NAN_METHOD(GetStatus) {
+        int statusCode = info[0]->IntegerValue();
+
+        Callback *callback = new Callback(info[1].As<Function>());
+        vector<string> messages;
+        messages.push_back("zero");
+        messages.push_back("one");
+        messages.push_back("two");
+        messages.push_back("three");
+
+        string statusMessageResult = getStatusMessage(statusCode, messages);
+
+        v8::Local<String> returnVal = Nan::New<String>(statusMessageResult).ToLocalChecked();
+        info.GetReturnValue().Set(returnVal);
+}
+
 NAN_MODULE_INIT(Init) {
         Nan::Set(target,
             New<String>("PassNumber").ToLocalChecked(),
@@ -32,6 +54,11 @@ NAN_MODULE_INIT(Init) {
         Nan::Set(target,
             New<String>("PassString").ToLocalChecked(),
             GetFunction(New<FunctionTemplate>(PassString))
+            .ToLocalChecked());
+
+        Nan::Set(target,
+            New<String>("GetStatus").ToLocalChecked(),
+            GetFunction(New<FunctionTemplate>(GetStatus))
             .ToLocalChecked());
 }
 
